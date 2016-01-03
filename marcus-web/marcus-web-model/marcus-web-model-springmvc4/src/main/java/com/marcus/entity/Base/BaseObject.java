05a83ee6e7d6a7aa5d4ef06c185d7a8d44c2a26f
Marcus Hua <846079865@qq.com>
2016-01-03 19:47:31 +0800

    更新

---
+++ marcus-web/marcus-web-model/marcus-web-model-springmvc4/src/main/java/com/marcus/entity/Base/BaseObject.java
@@ -0,0 +1,8 @@
+package com.marcus.entity.Base;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass 
+public class BaseObject implements java.io.Serializable{
+
+}
---
+++ marcus-web/marcus-web-model/marcus-web-model-springmvc4/src/main/java/com/marcus/entity/Role.java
@@ -0,0 +1,12 @@
+package com.marcus.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.marcus.entity.Base.BaseObject;
+
+@Entity
+@Table
+public class Role extends BaseObject implements java.io.Serializable{
+
+}
